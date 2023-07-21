d24153c2bbc4bd21b39021824b1117d568668f28
Matthew Charuel <matthew.charuel@epitech.eu>
2023-07-21 09:06:40 +0200

    add: API connect

--- step2/index.php
+++ step2/index.php
@@ -6,6 +6,19 @@
     <title>heyliotTest</title>
 </head>
 <body>
+    <?php
+    $api_key = 'YOUR_API_KEY';
+    $api_url = "https://api.thecatapi.com/v1/images/search?api_key=$api_key";
+    $response = file_get_contents($api_url);
+    $data = json_decode($response, true);
 
+    //list breed in tab for display after
+    if (is_array($data) && count($data) > 0) {
+        $image_url = $data[0]['url'];
+        echo "<img src='$image_url'>";
+    } else {
+        echo "Error img cat";
+    }
+    ?>
 </body>
 </html>
